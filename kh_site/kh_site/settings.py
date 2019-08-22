15b110ad46dd9766e53a4d46f067e64851f1e296
nhuntwalker <nhuntwalker@Gmail.com>
2019-08-22 02:05:04 -0700

    Updated settings.py to work based on configuration and changed the Groups to Roots of Restoration on the home page

--- kh_site/kh_site/settings.py
+++ kh_site/kh_site/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', ''))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split('/')
+if DEBUG:
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
+else:
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split('/')
 
 
 # Application definition
